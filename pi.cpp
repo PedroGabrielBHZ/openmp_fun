08f12b9b16adabd0656f3807a906fbf6d9bdca3b
Pedro <pedrogabrielbhz@gmail.com>
2025-08-21 18:14:09 -0300

    tweak a little...

--- pi.cpp
+++ pi.cpp
@@ -3,10 +3,10 @@
 #include <omp.h>
 #include <chrono>
 
-const long long num_steps = 1000000000; 
-double step;
+constexpr long long num_steps = static_cast<long long>(1e10);
+double step; 
 
-double sequential_pi() {
+double sequential_pi(long long num_steps, double step) {
     double sum = 0.0;
     for (long long i = 0; i < num_steps; ++i) {
         double x = (i + 0.5) * step;
@@ -15,7 +15,7 @@ double sequential_pi() {
     return step * sum;
 }
 
-double parallel_pi() {
+double parallel_pi(long long num_steps, double step) {
     double sum = 0.0;
     #pragma omp parallel for reduction(+:sum)
     for (long long i = 0; i < num_steps; ++i) {
@@ -26,15 +26,15 @@ double parallel_pi() {
 }
 
 int main() {
-    step = 1.0 / static_cast<double>(num_steps);
+    const double step = 1.0 / static_cast<double>(num_steps);
 
     auto start_seq = std::chrono::high_resolution_clock::now();
-    double pi_seq = sequential_pi();
+    auto pi_seq = sequential_pi(num_steps, step);
     auto end_seq = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_seq = end_seq - start_seq;
 
     auto start_par = std::chrono::high_resolution_clock::now();
-    double pi_par = parallel_pi();
+    auto pi_par = parallel_pi(num_steps, step);
     auto end_par = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> elapsed_par = end_par - start_par;
 
@@ -42,7 +42,9 @@ int main() {
     std::cout << "Sequential Pi: " << pi_seq << "\n";
     std::cout << "Sequential Time: " << elapsed_seq.count() << " seconds\n\n";
     std::cout << "Parallel Pi:   " << pi_par << "\n";
-    std::cout << "Parallel Time: " << elapsed_par.count() << " seconds\n";
+    std::cout << "Parallel Time: " << elapsed_par.count() << " seconds\n\n";
+    std::cout << "Speedup: " << elapsed_seq.count() / elapsed_par.count() << "x\n";
+
 
     return 0;
 }
